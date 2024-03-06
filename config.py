a21f6755789eed52e42e20292ee9f5a96b1537c9
Edward Buba <edward.buba@shara.co>
2024-03-06 02:48:58 +0100

    This change added the dotenv module to load environment variables, then modified the SQLAlchemy database URI to be dynamically generated based on those loaded values. This update allows for more flexibility in deployment settings, as the application can now manage different database configurations through the environment variables.

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+load_dotenv()
+
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@db:5432/${POSTGRES_DB}"
-    # SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
+    POSTGRES_USER = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_DB = os.getenv("POSTGRES_DB")
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@db:5432/{POSTGRES_DB}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
